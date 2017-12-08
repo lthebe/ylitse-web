6c9d968ff698a92f50ba397366f29ad7781807ce
Ivan Dolgov <ivan@qvarnlabs.com>
2017-12-08 11:19:05 +0200

    Move about dialog to nav bar

---
+++ src/Navigation.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AppBar from 'material-ui/AppBar';
+import Snackbar from 'material-ui/Snackbar';
+import { withStyles } from 'material-ui/styles';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import CloseIcon from 'material-ui-icons/Close';
+import InfoIcon from 'material-ui-icons/Info';
+
+import { version as uiVersion } from '../package.json';
+import About from './AboutDialog';
+
+const styles = () => ({
+    flex: {
+        flex: 1,
+    },
+    infoButton: {
+        marginLeft: 20,
+        marginRight: -12,
+    },
+});
+
+class Navigation extends Component {
+    static propTypes = {
+        classes: PropTypes.shape({
+            flex: PropTypes.string,
+            infoButton: PropTypes.string,
+        }).isRequired,
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            aboutOpen: false,
+            apiVersion: 'N/A',
+            errorOpen: false,
+            errorMessage: 'Error',
+        };
+    }
+
+    componentDidMount() {
+        this.fetchVersion();
+    }
+
+    fetchVersion = async () => {
+        try {
+            const resp = await fetch('http://127.0.0.1:8080/version');
+            const data = await resp.json();
+
+            this.setState({ apiVersion: data.api });
+        } catch (e) {
+            this.setState({ errorMessage: e.message });
+            this.openError();
+        }
+    }
+
+    openAbout = () => {
+        this.setState({ aboutOpen: true });
+    }
+
+    closeAbout = () => {
+        this.setState({ aboutOpen: false });
+    }
+
+    openError = () => {
+        this.setState({ errorOpen: true });
+    }
+
+    closeError = () => {
+        this.setState({ errorOpen: false });
+    }
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <AppBar position="static">
+                <Toolbar display="flex">
+                    <Typography
+                        type="title"
+                        color="inherit"
+                        className={classes.flex}
+                    >
+                        Ylitse Admin
+                    </Typography>
+                    <IconButton
+                        color="contrast"
+                        className={classes.infoButton}
+                        onClick={this.openAbout}
+                    >
+                        <InfoIcon />
+                    </IconButton>
+                </Toolbar>
+                <About
+                    open={this.state.aboutOpen}
+                    uiVersion={uiVersion}
+                    apiVersion={this.state.apiVersion}
+                    onOkClick={this.closeAbout}
+                    onRequestClose={this.closeAbout}
+                />
+                <Snackbar
+                    open={this.state.errorOpen}
+                    onRequestClose={this.closeError}
+                    message={this.state.errorMessage}
+                    action={[
+                        <IconButton color="inherit" onClick={this.closeError}>
+                            <CloseIcon />
+                        </IconButton>,
+                    ]}
+                />
+            </AppBar>
+        );
+    }
+}
+
+export default withStyles(styles)(Navigation);
--- src/Root.js
+++ src/Root.js
@@ -1,88 +1,14 @@
-import React, { Component } from 'react';
-import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton';
-import Snackbar from 'material-ui/Snackbar';
-import CloseIcon from 'material-ui-icons/Close';
-import InfoIcon from 'material-ui-icons/Info';
+import React from 'react';
+import { createMuiTheme, MuiThemeProvider } from 'material-ui/styles';
 
-import { version as uiVersion } from '../package.json';
-import About from './AboutDialog';
+import Navigation from './Navigation';
 
-class Root extends Component {
-    constructor(props) {
-        super(props);
+const theme = createMuiTheme();
 
-        this.state = {
-            aboutOpen: false,
-            apiVersion: 'N/A',
-            errorOpen: false,
-            errorMessage: 'Error',
-        };
-    }
-
-    componentDidMount() {
-        this.fetchVersion();
-    }
-
-    fetchVersion = async () => {
-        try {
-            const resp = await fetch('http://127.0.0.1:8080/version');
-            const data = await resp.json();
-
-            this.setState({ apiVersion: data.api });
-        } catch (e) {
-            this.setState({ errorMessage: e.message });
-            this.openError();
-        }
-    }
-
-    openAbout = () => {
-        this.setState({ aboutOpen: true });
-    }
-
-    closeAbout = () => {
-        this.setState({ aboutOpen: false });
-    }
-
-    openError = () => {
-        this.setState({ errorOpen: true });
-    }
-
-    closeError = () => {
-        this.setState({ errorOpen: false });
-    }
-
-    render() {
-        return (
-            <div>
-                <Button
-                    raised
-                    color="primary"
-                    onClick={this.openAbout}
-                >
-                    About
-                    <InfoIcon />
-                </Button>
-                <About
-                    open={this.state.aboutOpen}
-                    uiVersion={uiVersion}
-                    apiVersion={this.state.apiVersion}
-                    onOkClick={this.closeAbout}
-                    onRequestClose={this.closeAbout}
-                />
-                <Snackbar
-                    open={this.state.errorOpen}
-                    onRequestClose={this.closeError}
-                    message={this.state.errorMessage}
-                    action={[
-                        <IconButton color="inherit" onClick={this.closeError}>
-                            <CloseIcon />
-                        </IconButton>,
-                    ]}
-                />
-            </div>
-        );
-    }
-}
+const Root = () => (
+    <MuiThemeProvider theme={theme}>
+        <Navigation />
+    </MuiThemeProvider>
+);
 
 export default Root;
