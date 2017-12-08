cf4e6ee34e8483c46b40732f8cbff83c7c47c0d3
Ivan Dolgov <ivan@qvarnlabs.com>
2017-12-08 11:22:27 +0200

    Add profile sheet

--- src/Root.js
+++ src/Root.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { createMuiTheme, MuiThemeProvider } from 'material-ui/styles';
 
 import Navigation from './Navigation';
+import ProfileSheet from './ProfileSheet';
 
 const theme = createMuiTheme();
 
 const Root = () => (
     <MuiThemeProvider theme={theme}>
         <Navigation />
+        <ProfileSheet />
     </MuiThemeProvider>
 );
 
