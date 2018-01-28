db39bd2ad2506684fe3840ec5f09d20ba3d07e8d
Jonesus <joonas.palosuo@gmail.com>
2018-01-28 16:06:40 +0200

    Update ProfileForm tests

--- src/ProfileForm.test.js
+++ src/ProfileForm.test.js
@@ -1,36 +1,24 @@
 import React from 'react';
-import { createMount, createShallow } from 'material-ui/test-utils';
+import { shallow, mount } from 'enzyme';
 import ProfileForm from './ProfileForm';
 
 describe('ProfileForm', () => {
-    let mount;
-    let shallow;
-    beforeAll(() => {
-        mount = createMount();
-        shallow = createShallow();
-    });
-    afterAll(() => {
-        mount.cleanUp();
-        shallow.cleanUp();
-    });
-
     test('renders correctly', () => {
         // ProfileForm is a HOC, so diving
-        const shallowWrapper = shallow(<ProfileForm />).dive();
-        expect(shallowWrapper).toMatchSnapshot();
+        const wrapper = shallow(<ProfileForm />).dive();
+
+        expect(wrapper).toMatchSnapshot();
     });
 
     test('error message is shown upon post error', async () => {
         const wrapper = shallow(<ProfileForm />).dive();
         const error = 'post error';
-
         window.fetch = jest.fn();
         window.fetch.mockReturnValue({
-            json: () => {
-                throw new Error(error);
-            },
+            json: () => { throw new Error(error); },
         });
         const fakeEvent = { preventDefault: () => false };
+
         await wrapper.instance().sendProfile(fakeEvent);
         wrapper.update();
         expect(wrapper.find({ message: error, open: true })).toHaveLength(1);
@@ -44,13 +32,12 @@ describe('ProfileForm', () => {
         const username = 'testuser';
         const res = new Response(`{"username": "${username}"}`, {
             status: 200,
-            headers: {
-                'content-type': 'application/json',
-            },
+            headers: { 'content-type': 'application/json' },
         });
         window.fetch = jest.fn();
         window.fetch.mockReturnValue(Promise.resolve(res));
         const fakeEvent = { preventDefault: () => false };
+
         await wrapper.instance().sendProfile(fakeEvent);
         wrapper.update();
         expect(wrapper.find({
@@ -70,19 +57,19 @@ describe('ProfileForm', () => {
         const deepWrapper = mount(<ProfileForm />);
         const password = deepWrapper.find('PasswordField').find('input');
         password.instance().value = 'short';
+
         password.simulate('change');
         expect(deepWrapper.find('PasswordField')
             .find('FormHelperText').length).toBe(1);
     });
 
     test('username validation', () => {
-        // tests for user name validation
         const deepWrapper = mount(<ProfileForm />);
+
         const username = deepWrapper.find({
             name: 'username',
             label: 'Username',
         }).find('input');
-        // console.log(username.debug());
         username.instance().value = 'a';
         username.simulate('change');
         expect(deepWrapper.find({
@@ -98,7 +85,6 @@ describe('ProfileForm', () => {
     });
 
     test('screen name validation', () => {
-        // tests for user name validation
         const deepWrapper = mount(<ProfileForm />);
         // this is not the good way to find the element though
         // if label is changed for example
@@ -106,7 +92,6 @@ describe('ProfileForm', () => {
             name: 'nickname',
             label: 'Screen name',
         }).find('input');
-        // console.log(username.debug());
         nickname.instance().value = 'ab';
         nickname.simulate('change');
         expect(deepWrapper.find({
@@ -124,9 +109,6 @@ describe('ProfileForm', () => {
     test('skills chosen', () => {
         const deepWrapper = mount(<ProfileForm />);
         const skillsSelect = deepWrapper.find('SelectInput').find('input');
-        console.log(skillsSelect.debug());
-        // expect(skillsct.instance().state).to.have.property('onChange', {});
-        // skillsSelect.instance().setProps({ onChange: onChangeEvent });
         skillsSelect.simulate('change');
         // deepWrapper.update();
         // expect(onChangeEvent).toHaveBeenCalled();
