1f09336697c5b465e8e28b35ba911e7af9ec5f5e
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2018-01-28 15:09:26 +0200

    Fix checkboxgroup test

--- src/CheckboxGroup.test.js
+++ src/CheckboxGroup.test.js
@@ -3,29 +3,24 @@ import { shallow, mount } from 'enzyme';
 import CheckboxGroup from './CheckboxGroup';
 
 describe('CheckboxGroup', () => {
-    const clickEvent = jest.fn();
-    const languages = {
-        Finnish: false,
-        Swedish: false,
-        English: false,
-        Russian: false,
-    };
+    const mockFunc = jest.fn();
+    const languages = { Finnish: false };
     const props = {
         data: languages,
         className: '',
         label: 'Language',
-        onChange: clickEvent,
+        onChange: mockFunc,
     };
-    it('renders correctly', () => {
+
+    test('renders correctly', () => {
         const wrapper = shallow(<CheckboxGroup {...props} />);
         expect(wrapper).toMatchSnapshot();
     });
-    it('checkbox can be checked and unchecked', () => {
+
+    test('checkbox can be checked and unchecked', () => {
         const wrapper = mount(<CheckboxGroup {...props} />);
-        expect(clickEvent).not.toHaveBeenCalled();
-        const input = wrapper.find('FormControlLabel')
-            .find('[label="Finnish"]').find('input');
+        const input = wrapper.find('input[name="Finnish"]');
         input.simulate('change');
-        expect(clickEvent).toHaveBeenCalled();
+        expect(mockFunc).toHaveBeenCalled();
     });
 });
