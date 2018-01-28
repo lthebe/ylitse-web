b4bc271e02711a21a48f01694c7e565dc39d1b04
Jonesus <joonas.palosuo@gmail.com>
2018-01-28 16:30:12 +0200

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ const config = {
     verbose: true,
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.js'],
-    coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
-    setupTestFrameworkScriptFile: './enzyme.config.js',
+    coverageReporters: ['text-summary', 'html'],
+    setupFiles: ['./enzyme.config.js'],
 };
 
 module.exports = config;
