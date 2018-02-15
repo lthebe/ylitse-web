1a468377cf9fb2665547e3464c39986379f02ee8
laxmi thebe <laxmi.thebe@aalto.fi>
2018-02-15 02:27:22 +0200

    Implement Skill editor page

--- src/SkillsPage.js
+++ src/SkillsPage.js
@@ -1,17 +1,227 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
-
+import CloseIcon from 'material-ui-icons/Close';
+import Snackbar from 'material-ui/Snackbar';
+import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import TextField from 'material-ui/TextField';
+import DeleteIcon from 'material-ui-icons/Delete';
 import Page from './Page';
 
-const SkillsPage = () => (
-    <Page>
-        <Typography variant="headline" component="h3">
-            Mentor skills
-        </Typography>
-        {/*
-        <SkillsList />
-        */}
-    </Page>
-);
+const styles = theme => ({
+    row: {
+        marginTop: theme.spacing.unit * 2,
+        marginRight: theme.spacing.unit,
+    },
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+        width: 500,
+        height: 500,
+        transform: 'translateZ(0)',
+    },
+    titleBar: {
+        background:
+      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    icon: {
+        color: 'white',
+    },
+});
 
-export default SkillsPage;
+class SkillsPage extends Component {
+    static propTypes = {
+        classes: PropTypes.shape({
+            root: PropTypes.string,
+            gridList: PropTypes.string,
+            titleBar: PropTypes.string,
+            icon: PropTypes.string,
+        }).isRequired,
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            errors: {},
+            errorOpen: false,
+            errorMessage: '',
+            newSkill: '',
+            skills: [],
+        };
+    }
+    componentDidMount() {
+        this.fetchSkills();
+    }
+
+    fetchSkills = async () => {
+        try {
+            // basically, all are same apiUrl, can I move somewhere?
+            const apiUrl = process.env.API_URL || 'http://127.0.0.1:8080';
+            const resp = await fetch(`${apiUrl}/skills`);
+            const data = await resp.json();
+            this.setState({ skills: data.resources });
+        } catch (e) {
+            this.openError(e.message);
+        }
+    };
+
+    sendSkill = async (skill) => {
+        const apiUrl = process.env.API_URL || 'http://127.0.0.1:8080';
+        try {
+            await fetch(`${apiUrl}/skills`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'POST',
+                body: JSON.stringify(skill),
+            });
+            this.fetchSkills();
+        } catch (e) {
+            this.openError(e.message);
+        }
+    }
+
+    openError = (errorMessage) => {
+        this.setState({ errorMessage, errorOpen: true });
+    }
+
+    closeError = () => {
+        this.setState({ errorOpen: false });
+    }
+
+    deleteSkill = async (id) => {
+        const apiUrl = process.env.API_URL || 'http://127.0.0.1:8080';
+        try {
+            await fetch(`${apiUrl}/skills/${id}`, {
+                method: 'DELETE',
+            });
+            this.setState(prevState => ({
+                skills: prevState.skills.filter(s => s.id !== id),
+            }));
+        } catch (e) {
+            this.openError(e.message);
+        }
+    }
+
+    addSkill = (event) => {
+        switch (event.key) {
+            case 'Enter': {
+                if (this.state.errors.newSkill || !this.state.newSkill) {
+                    return;
+                }
+                this.sendSkill({ name: this.state.newSkill });
+                this.setState({
+                    newSkill: '',
+                });
+                break;
+            }
+            case 'Escape': {
+                this.setState(prevState => ({
+                    newSkill: '',
+                    errors: { ...prevState.errors, newSkill: '' },
+                }));
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    updateValue = ({ target }) => {
+        const { name, value } = target;
+        const { errors } = this.state;
+        let valid;
+        switch (name) {
+            case 'newSkill':
+                valid = !(this.state.skills
+                    .some(skill =>
+                        (skill.name.toLowerCase() === value.toLowerCase())));
+                errors.newSkill = valid ? '' : 'Already in list';
+                break;
+            default:
+                break;
+        }
+        this.setState({
+            [target.name]: target.value,
+            errors,
+        });
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Page>
+                <Typography variant="headline" component="h3">
+                 Mentor skills
+                </Typography>
+                <TextField
+                    name="newSkill"
+                    label="Add a skill"
+                    className={classes.row}
+                    value={this.state.newSkill}
+                    error={Boolean(this.state.errors.newSkill)}
+                    helperText={this.state.errors.newSkill}
+                    onChange={this.updateValue}
+                    onKeyDown={this.addSkill}
+                />
+
+                <div className={classes.root}>
+                    <GridList
+                        cellHeight={60}
+                        spacing={1}
+                        className={classes.gridList}
+                    >
+                        {this.state.skills
+                            .filter(skill =>
+                                skill.name.toLowerCase()
+                                    .includes(this.state.newSkill
+                                        .toLowerCase()))
+                            .map(skill => (
+                                <GridListTile
+                                    key={skill.id}
+                                    cols={0.66}
+                                >
+                                    <GridListTileBar
+                                        title={skill.name}
+                                        titlePosition="top"
+                                        actionIcon={
+                                            <IconButton
+                                                onClick={() => {
+                                                    this.deleteSkill(skill.id);
+                                                }}
+                                                className={classes.icon}
+                                            >
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        }
+                                        actionPosition="left"
+                                        className={classes.titleBar}
+                                    />
+                                </GridListTile>
+                            ))}
+                    </GridList>
+                </div>
+                <Snackbar
+                    open={this.state.errorOpen}
+                    message={this.state.errorMessage}
+                    autoHideDuration={3000}
+                    action={
+                        <IconButton color="inherit" onClick={this.closeError}>
+                            <CloseIcon />
+                        </IconButton>
+                    }
+                    onClose={this.closeError}
+                />
+            </Page>
+        );
+    }
+}
+
+export default withStyles(styles)(SkillsPage);
